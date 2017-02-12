d0beb8827430369176fc6ead99126133da881c27
broschke <bernardoroschke@gmail.com>
2017-02-12 18:48:33 -0500

    fix

--- prob.py
+++ prob.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 x = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 9, 9]
 
 plt.boxplot(x)
-plt.show()
+#plt.show()
 plt.savefig("boxplot.png")
 
 plt.hist(x, histtype='bar')
-plt.show()
+#plt.show()
 plt.savefig("hist.png")
 
 graph1 = stats.probplot(x, dist="norm", plot=plt)
-plt.show()
+#plt.show()
 plt.savefig("qq.png")
 
